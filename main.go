55c9d69c6b1ecb30c1c74fe9b7cdbe13394c202e
Prashanth Selvam <prashanth.selvam@gmail.com>
2025-05-20 23:08:57 -0400

    Set a custom status code

--- main.go
+++ main.go
@@ -38,6 +38,10 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreatePost handler function
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// Use the w.WriteHeader() method to send a 201 status code.
+	w.WriteHeader(201)
+
+	// Then use w.Write() method to write the response body as normal.
 	w.Write([]byte("Save a new snippet..."))
 }
 
