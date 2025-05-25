23532c412197fe4d1083d7b78a0103c03eec1e33
Prashanth Selvam <prashanth.selvam@gmail.com>
2025-05-25 09:25:13 -0400

    Refactor routes into its own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,29 +39,13 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// Register the other application routes as normal.
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	// Use the Info() method to log the starting server message at Info severity (along
 	// with the listen address as an attribute)
 	logger.Info("Starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe()
+	err := http.ListenAndServe(*addr, app.routes())
 
 	// And we also use the Error() method to log any error message returned by
 	// http.ListenAndServe() at Error severity (with no additional attributes),
