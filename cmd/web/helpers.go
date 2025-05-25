955d79d488fabb708cd1dd59b0c130eff7c92f19
Prashanth Selvam <prashanth.selvam@gmail.com>
2025-05-25 09:21:43 -0400

    Include stacktrace in serverError handler

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"runtime/debug"
 )
 
 // The serverError helper writes a log entry at Error level (including the request
@@ -12,11 +13,13 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
+		trace  = string(debug.Stack())
 	)
 
 	app.logger.Error(err.Error(),
 		slog.String("method", method),
 		slog.String("uri", uri),
+		slog.String("trace", trace),
 	)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
